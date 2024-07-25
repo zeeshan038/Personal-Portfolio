40a723238fad324ae52657078c272e627785e93a
zeeshan038 <hankhan123623@gmail.com>
2024-07-25 20:05:09 +0500

    project added

--- src/projectsdata.js
+++ src/projectsdata.js
@@ -1,14 +1,14 @@
-import blogApp from './assets/blog.png'
+import blogApp from './assets/mern-blog.png'
  
  export const  projectData =[
     {
         id : 1 ,
         image : blogApp ,
         category : 'mern',
-        h1 : 'Admin Dashboard',
+        h1 : 'Blog App',
         tech1 : "MERN",
         tech2 : "Tailwind",
-        urlCode : 'https://github.com/zeeshan038/mern-blog-',
+        urlCode : 'https://github.com/zeeshan038/mern-blog-Refined',
         urlLive : "",
 
     },
